5c69250477290050c82823e3b0228fbdbb88d25c
arunman1kandan <arunm291003@gmail.com>
2022-10-10 14:19:35 +0530

    Fixed issues

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -10,16 +10,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
->>>>>>> 9212bdaabe56751ef231df7edcf22b652fef73cc
